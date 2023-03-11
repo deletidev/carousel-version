48ada8fa200bcc4d31254c859c939ba2eef9e58d
LeticiadelaOsa <leticiablabla1@gmail.com>
2023-03-11 20:46:19 +0100

    start

--- src/js/index.js
+++ src/js/index.js
@@ -1,9 +1,37 @@
 // El styles lo importamos aquí, ya se carga después al compilar todo
 import '../scss/styles.scss';
-import { changeHeader, closeMenu, toggleMenu } from './menu';
 
-// import spockImg from '../assets/images/icon-spock.svg'; en caso de necesitar rutas de las imágenes
+let options = {
+  root: document.querySelector('#scrollArea'),
+  rootMargin: '0px',
+  threshold: [0.5]
+  //   delay: 0
+};
 
-const botonMenu = document.getElementById('boton-menu');
+let callback = (entries, observer) => {
+  entries.forEach(entry => {
+    if (entry.isIntersecting) {
+      entry.target.classList.add('active');
+      entry.target.dataset.color = 'purple';
+      let activos = document.querySelectorAll('.active');
 
-botonMenu.addEventListener('click', () => toggleMenu(botonMenu));
+      if (activos.length === 3 || activos.length === 4) {
+        activos[0].dataset.color = 'green';
+        activos[1].dataset.color = 'pink';
+        activos[2].dataset.color = 'purple';
+        activos[2].nextElementSibling.dataset.color = 'pink';
+        activos[2].nextElementSibling.nextElementSibling.dataset.color =
+          'green';
+      }
+    } else {
+      entry.target.classList.remove('active');
+      entry.target.dataset.color = 'green';
+    }
+  });
+};
+
+let observer = new IntersectionObserver(callback, options);
+let target = document.querySelectorAll('.carousel__item--grid');
+target.forEach(target => {
+  observer.observe(target);
+});
