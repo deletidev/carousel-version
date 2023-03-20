20201d70ce0359caae29997728d58c02405b0927
LeticiadelaOsa <leticiablabla1@gmail.com>
2023-03-20 19:17:45 +0100

    js

--- src/js/index.js
+++ src/js/index.js
@@ -1,8 +1,43 @@
-// El styles lo importamos aquí, ya se carga después al compilar todo
 import '../scss/styles.scss';
 
+//Comun functions
+
+const deactivateButton = btn => {
+  btn.disabled = true;
+};
+
+const activateButton = btn => {
+  btn.disabled = false;
+};
+
+const nextSlide = (array, scroll) => {
+  const movScroll = array[0].clientWidth;
+  scroll.scrollLeft += movScroll;
+};
+
+const prevSlide = (array, scroll) => {
+  const movScroll = array[0].clientWidth;
+  scroll.scrollLeft -= movScroll;
+};
+
+//carousel simple
+const areaTwo = document.getElementById('scrollArea2');
+let itemsSimple = document.querySelectorAll('.carousel__item');
+const simplePrev = document.getElementById('simple-prev');
+const simpleNext = document.getElementById('simple-next');
+
+simpleNext.addEventListener('click', () => {
+  nextSlide(itemsSimple, areaTwo);
+});
+simplePrev.addEventListener('click', () => {
+  prevSlide(itemsSimple, areaTwo);
+});
+
+//carousel version
 const area = document.getElementById('scrollArea');
 const colores = ['green', 'pink', 'purple'];
+const verionPrev = document.getElementById('version-prev');
+const versionNext = document.getElementById('version-next');
 
 let options = {
   root: area,
@@ -21,6 +56,16 @@ let callback = (entries, observer) => {
           activo.dataset.color = colores[index];
         });
       }
+      if (!activos[0].previousElementSibling) {
+        deactivateButton(verionPrev);
+      } else {
+        activateButton(verionPrev);
+      }
+      if (!entry.target.nextElementSibling) {
+        deactivateButton(versionNext);
+      } else {
+        activateButton(versionNext);
+      }
     } else {
       entry.target.classList.remove('active');
       entry.target.dataset.color = 'green';
@@ -33,4 +78,11 @@ let target = document.querySelectorAll('.carousel__item--grid');
 target.forEach(target => {
   target.classList.remove('active');
   observer.observe(target);
+});
+
+versionNext.addEventListener('click', () => {
+  nextSlide(target, area);
+});
+verionPrev.addEventListener('click', () => {
+  prevSlide(target, area);
 });
